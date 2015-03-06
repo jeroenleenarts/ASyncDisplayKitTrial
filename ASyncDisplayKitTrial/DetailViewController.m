d2242b915998ca1e76418dae20b4a96060566ec1
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2015-03-06 20:14:22 +0100

    Only proces data if no error occurred.

--- ASyncDisplayKitTrial/DetailViewController.m
+++ ASyncDisplayKitTrial/DetailViewController.m
@@ -14,23 +14,25 @@
 
 -(void)viewWillAppear:(BOOL)animated {
     self.infoLabel.text = NSLocalizedString(@"Loading detail image", nil);
-    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:self.imageInfo.URL] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        NSLog(@"Finished downloading detail image");
-        dispatch_async(dispatch_get_main_queue(), ^{
-            
-            if ([(NSHTTPURLResponse*)response statusCode] != 200) {
-                self.infoLabel.text = NSLocalizedString(@"Detail image unavailable.", nil);
-                return;
-            }
-            UIImage *image = [UIImage imageWithData:data];
-            if (image == nil) {
-                self.infoLabel.text = NSLocalizedString(@"Image data corrupt", nil);
-                return;
-            }
-            self.imageView.image = image;
-            self.infoLabel.hidden = YES;
-        });
-        NSLog(@"Set image");
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:[self.imageInfo.URLString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (!error) {
+            NSLog(@"Finished downloading detail image");
+            dispatch_async(dispatch_get_main_queue(), ^{
+
+                if ([(NSHTTPURLResponse*)response statusCode] != 200) {
+                    self.infoLabel.text = NSLocalizedString(@"Detail image unavailable.", nil);
+                    return;
+                }
+                UIImage *image = [UIImage imageWithData:data];
+                if (image == nil) {
+                    self.infoLabel.text = NSLocalizedString(@"Image data corrupt", nil);
+                    return;
+                }
+                self.imageView.image = image;
+                self.infoLabel.hidden = YES;
+            });
+            NSLog(@"Set image");
+        }
     }];
     
     [task resume];
